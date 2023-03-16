f935a1e8b55f4aa218bb6cd307731791fb1149f8
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-03-16 08:52:48 +0700

    Create HocSinhChuyenToan.java

---
+++ LTHDT_LAP/src/LAB_06/Cau_02/HocSinhChuyenToan.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LAB_06.Cau_01;
+
+/**
+ *
+ * @author TAMTRAN
+ */
+public class HocSinhChuyenToan extends HocSinh {
+    
+    public HocSinhChuyenToan(String hoTen, String lop, float toan, float ly, float hoa) {
+       super(hoTen, lop, toan, ly, hoa);
+    }
+
+    @Override
+    public float diemTrungBinh (){
+    return (toan*2+ly+hoa)/4;
+    }
+
+}
