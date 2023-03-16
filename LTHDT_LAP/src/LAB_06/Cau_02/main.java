7eb5f1e2b75aebfa226f41c861970e2265c72c59
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-03-16 09:03:31 +0700

    Create main.java

---
+++ LTHDT_LAP/src/LAB_06/Cau_02/main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LAB_06.Cau_01;
+
+/**
+ *
+ * @author TAMTRAN
+ */
+public class main {
+   public static void main (String [] args){
+      HocSinh hs = new HocSinh();
+      hs.diemTrungBinh();
+
+      HocSinhChuyenToan hs1 = new HocSinhChuyenToan();
+      hs1.diemTrungBinh();
+   }
+}
+   
