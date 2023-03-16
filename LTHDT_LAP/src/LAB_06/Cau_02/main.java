5afbe8fd086805b39b5e57369e76c6a6293c4327
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-03-16 18:36:33 +0700

    Update main.java

--- LTHDT_LAP/src/LAB_06/Cau_02/main.java
+++ LTHDT_LAP/src/LAB_06/Cau_02/main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package LAB_06.Cau_01;
+package LAB_06.Cau_02;
 
 /**
  *
@@ -10,11 +10,11 @@ package LAB_06.Cau_01;
  */
 public class main {
    public static void main (String [] args){
-      HocSinh hs = new HocSinh();
-      hs.diemTrungBinh();
+      
+      HocSinh hs=new HocSinh("Vy", "10A2", 8, 9, 6);
+      System.out.println( "Diem cua hoc sinh la " + hs.diemTrungBinh());
 
-      HocSinhChuyenToan hs1 = new HocSinhChuyenToan();
-      hs1.diemTrungBinh();
+      HocSinhChuyenToan hs1 = new HocSinhChuyenToan("Hoa", "10A6", 10, 7, 8);
+      System.out.println( "Diem cua hoc sinh chuyen toan la " + hs1.diemTrungBinh());
    }
 }
-   
