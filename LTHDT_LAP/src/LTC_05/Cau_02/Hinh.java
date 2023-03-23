861b808e33fe79ff07a9b3bad22664612ac1c07c
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-03-23 08:10:45 +0700

    Create Hinh.java

---
+++ LTHDT_LAP/src/LTC_05/Cau_02/Hinh.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LTC_05.Cau_02;
+
+/**
+ *
+ * @author TAMTRAN
+ */
+public class Hinh {
+    private double dienTich;
+    private PI=(float)Math.PI;
+    public Hinh() {
+        this.dienTich=0;
+    }
+
+    public Hinh(double dienTich) {
+        this.dienTich=dienTich;
+    }
+
+    public double dienTich() {
+        return dienTich;
+    }
+
+    public void dienTich(double dienTich) {
+        this.dienTich = dienTich;
+    }
