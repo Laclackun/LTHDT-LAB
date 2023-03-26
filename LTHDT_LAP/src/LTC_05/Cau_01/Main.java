7f52992202821ab603379634d829d1b1144c0fea
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-03-26 21:45:26 +0700

    Update Main.java

--- LTHDT_LAP/src/LTC_05/Cau_01/Main.java
+++ LTHDT_LAP/src/LTC_05/Cau_01/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package LAB_06.Cau_01;
+package LTC_05.Cau_01;
 
 /**
  *
@@ -11,14 +11,13 @@ package LAB_06.Cau_01;
 public class Main {
     public static void main (String [] args){
       
-      ChuyenXe nv = new ChuyenXe("An Nam", 8164, 4449, 5000);
+      ChuyenXe cx = new ChuyenXe("An Nam", 8164, 4449, 5000);
+      cx.thongTinChuyen();
       
-      ChuyenXeNgoaiThanh cxngoai = new ChuyenXeNgoaiThanh("Hòa An", 18164, 2149, 15000, "Chính Thành", 4);
+      ChuyenXeNgoaiThanh cxngoai = new ChuyenXeNgoaiThanh("Chính Thành", 4, "Hòa An", 18164, 2149, 15000);
       cxngoai.thongTinChuyen();
-      cxngoai.NoiDen();
       
-      ChuyenXeNoiThanh cxnoi = new ChuyenXeNoiThanh("Thanh Hóa", 1423, 1149, 45000, "A23HU", 12);
+      ChuyenXeNoiThanh cxnoi = new ChuyenXeNoiThanh("A23HU", 12, "Thanh Hóa", 1423, 1149, 45000);
       cxnoi.thongTinChuyen();
-      cxnoi.SoTuyen();
    }
 }
