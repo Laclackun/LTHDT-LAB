867926ebafd03f0196e6877876b72868864ea654
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-04-06 23:55:06 +0700

    Create PhuongTienDiChuyen.java

---
+++ LTHDT_LAP/src/LAB_07/Cau_01/PhuongTienDiChuyen.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LAB_07.Cau_01;
+
+/**
+ *
+ * @author TAMTRAN
+ */
+
+abstract class PhuongTienDiChuyen {
+    String loaiPhuongTIen;
+  
+}
