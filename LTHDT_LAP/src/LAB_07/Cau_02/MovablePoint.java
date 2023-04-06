343889f1ae993bd1f7d923ff923f34777ce5bfcc
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-04-06 23:37:22 +0700

    Create MovablePoint.java

---
+++ LTHDT_LAP/src/LAB_07/Cau_02/MovablePoint.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LAB_07.Cau_02;
+
+/**
+ *
+ * @author TAMTRAN
+ */
+
+public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public void moveUp() {
+        System.out.println ("Xe Di Len Phia Truoc");
+    }
+
+    @Override
+    public void moveDown() {
+        System.out.println ("Xe Di Lui Phia Sau");
+    }
+
+    @Override
+    public void moveLeft() {
+        System.out.println ("Xe Di Sang Trai");
+    }
+
+    @Override
+    public void moveRight() {
+        System.out.println ("Xe Di Sang Phai");
+    }
+
+    @Override    
+    public String toString() {
+        return "X = " + x + " Y = " + y + " X co toc do la: " + xSpeed + " Y co toc do la: " + ySpeed;
