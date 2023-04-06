947644594af8b2848509d4db0e8abd4c51af7576
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-04-06 08:00:43 +0700

    Create Movable.java

---
+++ LTHDT_LAP/src/LAB_07/Cau_02/Movable.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LAB_07.Cau_02;
+
+/**
+ *
+ * @author TAMTRAN
+ */
+
+public interface Movable {
+    public void moveUp();
+    public void moveDown();
+    public void moveLeft();
+    public void moveRight();
+}
