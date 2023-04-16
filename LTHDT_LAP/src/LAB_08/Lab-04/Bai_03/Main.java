9c7e0c45acbcc5e412ae6c878aaac575de22956e
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-04-17 06:51:54 +0700

    Create Main.java

---
+++ LTHDT_LAP/src/LAB_08/Lab-04/Bai_03/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        SanPham sp1 = new SanPham("SP1", 5000, 1000);
+        SanPham sp2 = new SanPham("SP2", 10000);
+        sp1.xuat();
+        sp2.xuat();
+    }
+}
