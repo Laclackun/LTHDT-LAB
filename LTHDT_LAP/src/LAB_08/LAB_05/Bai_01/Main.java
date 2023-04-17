495e98150093c9b17aff507531a150cc290abafb
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-04-17 07:50:02 +0700

    Add files via upload

---
+++ LTHDT_LAP/src/LAB_08/LAB_05/Bai_01/Main.java
@@ -0,0 +1,10 @@
+package com.polyit;
+
+public class Main {
+    public static void main(String[] args) {
+        MangSoThuc ds = new MangSoThuc();
+        ds.nhap();
+        ds.hienThi();
+        ds.tinhTong();
+    }
+}
