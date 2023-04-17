095b8897f899b0d74dd36d90e5168c14f5b6a1aa
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-04-17 19:27:55 +0700

    Create XeOto.java

---
+++ LTHDT_LAP/src/LAB_07/Cau_01/XeOto.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LAB_07.Cau_01;
+
+/**
+ *
+ * @author TAMTRAN
+ */
+
+public class XeOto extends PhuongTienDiChuyen{
+    private String loaiNhienLieu;
+
+    public XeOto(String loaiNhienLieu, HangSanXuat hangSanXuat) {
+        super("O to", hangSanXuat);
+        this.loaiNhienLieu = loaiNhienLieu;
+    }
+
+    public String getLoaiNhienLieu() {
+        return loaiNhienLieu;
+    }
+
+    public void setLoaiNhienLieu(String loaiNhienLieu) {
+        this.loaiNhienLieu = loaiNhienLieu;
+    }
+    
+    @Override
+    public double layVanToc(){
+        return 120;
+    }
+}
