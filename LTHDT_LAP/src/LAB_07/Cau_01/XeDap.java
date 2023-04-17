3923345a35ab219b1aa004d65d5c6e436442564d
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-04-17 19:27:20 +0700

    Update XeDap.java

--- LTHDT_LAP/src/LAB_07/Cau_01/XeDap.java
+++ LTHDT_LAP/src/LAB_07/Cau_01/XeDap.java
@@ -14,12 +14,9 @@ public class XeDap extends PhuongTienDiChuyen{
     public XeDap(HangSanXuat hangSanXuat) {
         super("Xe dap", hangSanXuat);
     }
-
     
-
     @Override
     public double layVanToc(){
         return 30;
     }
-    
 }
