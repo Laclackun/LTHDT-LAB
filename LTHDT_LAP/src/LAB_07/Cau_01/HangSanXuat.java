36f09ef711cb124d60df5f682f91774fca4bedd4
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-04-17 19:29:30 +0700

    Create HangSanXuat.java

---
+++ LTHDT_LAP/src/LAB_07/Cau_01/HangSanXuat.java
@@ -0,0 +1,38 @@
+*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LAB_07.Cau_01;
+
+/**
+ *
+ * @author TAMTRAN
+ */
+
+public class HangSanXuat {
+    public String tenHangSanXuat;
+    public String tenQuocGia;
+
+    public HangSanXuat(String tenHangSanXuat, String tenQuocGia) {
+        this.tenHangSanXuat = tenHangSanXuat;
+        this.tenQuocGia = tenQuocGia;
+    }
+    
+    public String getTenHangSanXuat() {
+        return tenHangSanXuat;
+    }
+
+    public void setTenHangSanXuat(String tenHangSanXuat) {
+        this.tenHangSanXuat = tenHangSanXuat;
+    }
+
+    public String getTenQuocGia() {
+        return tenQuocGia;
+    }
+
+    public void setTenQuocGia(String tenQuocGia) {
+        this.tenQuocGia = tenQuocGia;
+    }
+
+    
+}
