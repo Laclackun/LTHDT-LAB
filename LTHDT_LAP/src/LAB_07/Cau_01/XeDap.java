4dec28bf028765daf1330cf2dde610040b5dba41
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-04-17 19:27:06 +0700

    Create XeDap.java

---
+++ LTHDT_LAP/src/LAB_07/Cau_01/XeDap.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LAB_07.Cau_01;
+
+/**
+ *
+ * @author TAMTRAN
+ */
+
+public class XeDap extends PhuongTienDiChuyen{
+
+    public XeDap(HangSanXuat hangSanXuat) {
+        super("Xe dap", hangSanXuat);
+    }
+
+    
+
+    @Override
+    public double layVanToc(){
+        return 30;
+    }
+    
+}
