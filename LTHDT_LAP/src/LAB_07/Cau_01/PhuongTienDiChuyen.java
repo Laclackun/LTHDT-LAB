21b1759301321f8758d6484e4b5ff552fc8cce7b
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-04-17 19:26:25 +0700

    Update PhuongTienDiChuyen.java

--- LTHDT_LAP/src/LAB_07/Cau_01/PhuongTienDiChuyen.java
+++ LTHDT_LAP/src/LAB_07/Cau_01/PhuongTienDiChuyen.java
@@ -10,6 +10,27 @@ package LAB_07.Cau_01;
  */
 
 abstract class PhuongTienDiChuyen {
-    String loaiPhuongTIen;
-  
+    protected String loaiPhuongTien;
+    protected HangSanXuat hangSanXuat;
+
+    public PhuongTienDiChuyen(String loaiPhuongTien, HangSanXuat hangSanXuat) {
+        this.loaiPhuongTien = loaiPhuongTien;
+        this.hangSanXuat = hangSanXuat;
+    }
+    
+    
+    public String layTenHangSanXuat(){
+        return this.hangSanXuat.getTenHangSanXuat();
+    }
+    
+    public void batDau(){
+        System.out.println("Bat dau");
+    }
+    public void tangtoc(){
+        System.out.println("Tang toc");
+    }
+    public void dungLai(){
+        System.out.println("Dung lai");
+    }
+    abstract double layVanToc(); 
 }
