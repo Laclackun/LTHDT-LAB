165f30ea7e24f5a1d36ddd43df0dd37e8b215fe5
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-04-27 15:18:16 +0700

    Update Xe.java

--- ThucHanh/LAB_01/Cau_1A/Xe.java
+++ ThucHanh/LAB_01/Cau_1A/Xe.java
@@ -1,34 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LAB_01.Cau_1A;
+
+/**
+ *
+ * @author TNMT
+ */
 public class Xe {
-private String tenChuXe:
-private String loaiXe:
-private int dungTich;
-private float triGia;
+    private String tenChuXe;
+    private String loaiXe;
+    private int dungTich;
+    private float triGia;
+    
+    public Xe() {
+        this.tenChuXe = null;
+        this.loaiXe = null;
+        this.dungTich = 0;
+        this.triGia = 0;
+    }
 
-public Xe() {
-this.tenChuxe = null;
-this. loaixe = null;
-this.dungTich = 0;
-this.triGia = 0;
+    public Xe(String tenChuXe, String loaiXe, int dungTich, float triGia) {
+        this.tenChuXe = tenChuXe;
+        this.loaiXe = loaiXe;
+        this.dungTich = dungTich;
+        this.triGia = triGia;
+    }
 
-public Xe (String tenChuXe, String loaiXe, ịt dungTich, float triGia) {
-this.tenChuXe = tenChuXe;
-this.triGia = triGia;
-this.loaixe = loaixe;
-this.dungTich = dungTich;
+    public String getTenChuXe() {
+        return tenChuXe;
+    }
 
-public String getTenChuXe () { 
-return tenChuXe;
+    public void setTenChuXe(String tenChuXe) {
+        this.tenChuXe = tenChuXe;
+    }
+
+    public String getLoaiXe() {
+        return loaiXe;
+    }
+
+    public void setLoaiXe(String loaiXe) {
+        this.loaiXe = loaiXe;
+    }
+
+    public int getDungTich() {
+        return dungTich;
+    }
+
+    public void setDungTich(int dungTich) {
+        this.dungTich = dungTich;
+    }
+
+    public float getTriGia() {
+        return triGia;
+    }
+
+    public void setTriGia(float triGia) {
+        this.triGia = triGia;
+    }
+    
+
+    public float tinhThue() {
+        if (dungTich<100) 
+            return (triGia * (1/100));
+                    
+        if (dungTich >=100 & dungTich <= 200)
+            return (triGia * (3/100));
+                    
+        else return (triGia * (5/100));
+    }
+
+    @Override
+    public String toString() {
+        return "Tên Chủ Xe      "  + "Loại Xe      " + "Dung Tích      " + "Trị Giá      " + "Thuế Phải Nộp" ;
+    }
+
+    public void DaiPhanCach() {
+        System.out.printf("=========================================================================================");
+    }
+
+    void inThongTinXe(){
+        System.out.printf("%20s %10s %d %10.2f %8.2f", tenChuXe, loaiXe, dungTich, triGia, tinhThue());
+    }
 }
-
-public void setTenChuXe (String tenChuxe) { 
-this.tenChuXe = tenChuXe;
-}
-
-public String getLoaiXe () {
-return loaiXe;
-}
-
-public void setLoaiXe (String loaiXe) { 
-this.loaiXe = loaiXe;
-}
-
