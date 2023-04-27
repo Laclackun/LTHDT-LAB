e5a48fdc26c848589960a9e0e8d7842eb9f812c4
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-04-27 15:09:45 +0700

    Update Xe.java

--- ThucHanh/LAB_01/Cau_1A/Xe.java
+++ ThucHanh/LAB_01/Cau_1A/Xe.java
@@ -1,1 +1,34 @@
-.
+public class Xe {
+private String tenChuXe:
+private String loaiXe:
+private int dungTich;
+private float triGia;
+
+public Xe() {
+this.tenChuxe = null;
+this. loaixe = null;
+this.dungTich = 0;
+this.triGia = 0;
+
+public Xe (String tenChuXe, String loaiXe, ịt dungTich, float triGia) {
+this.tenChuXe = tenChuXe;
+this.triGia = triGia;
+this.loaixe = loaixe;
+this.dungTich = dungTich;
+
+public String getTenChuXe () { 
+return tenChuXe;
+}
+
+public void setTenChuXe (String tenChuxe) { 
+this.tenChuXe = tenChuXe;
+}
+
+public String getLoaiXe () {
+return loaiXe;
+}
+
+public void setLoaiXe (String loaiXe) { 
+this.loaiXe = loaiXe;
+}
+
