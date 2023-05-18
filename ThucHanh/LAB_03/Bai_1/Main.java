9b66f39337668ccb699b5d15f60c75ad33bad9e8
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-05-18 16:00:34 +0700

    Update Main.java

--- ThucHanh/LAB_03/Bai_1/Main.java
+++ ThucHanh/LAB_03/Bai_1/Main.java
@@ -5,6 +5,7 @@
 package LAB_03.Bai_1;
 import java.time.LocalDate;
 
+
 /**
  *
  * @author TNMT
@@ -15,12 +16,19 @@ public class Main {
         Product p2 = new Product("Gao", "sp1", 18);
         Product p3 = new Product("Duong", "sp3", 10);
         Product p4 = new Product("Gao", "sp1", 18);
+        Product p5 = new Product("Nuoc Mam", "sp2", 12);
+        Product p6 = new Product("Dau Hao", "sp5", 17);
+        Product p7 = new Product("Tuong Ca", "sp1", 6);
+        
         
         Order order = new Order (1, LocalDate.now());
         order.addLineItem(p1, 10);
         order.addLineItem(p2, 5);
         order.addLineItem(p3, 1);
         order.addLineItem(p4, 1);
+        order.addLineItem(p5, 2);
+        order.addLineItem(p6, 3);
+        order.addLineItem(p7, 4);
         
         System.out.println(order.toString());
     }
