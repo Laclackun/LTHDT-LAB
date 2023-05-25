412a657219be1f6c45a6b84dab99cf6ba7fb9c88
Bảo Trân <125117839+Laclackun@users.noreply.github.com>
2023-05-25 17:08:26 +0700

    Update QuanLyPhongHoc.java

--- ThucHanh/LAB_05/Bai_1/QuanLyPhongHoc.java
+++ ThucHanh/LAB_05/Bai_1/QuanLyPhongHoc.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
-class QuanLyPhongHoc {
+public class QuanLyPhongHoc {
     private List<PhongHoc> danhSachPhong;
 
     public QuanLyPhongHoc() {
